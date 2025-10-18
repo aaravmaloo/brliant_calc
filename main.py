408bbd381c62b184c66a15afaeb0c26a43ca719b
Aarav Maloo <aaravmaloo06@gmail.com>
2025-10-18 19:42:14 +0530

    Add argparse CLI for add

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import argparse
+from calculator import basic_ops
+
+parser = argparse.ArgumentParser(description="A command-line advanced scientific calculator")
+
+subparser = parser.add_subparsers(dest="command", help="available commands")
+
+basic_parser = subparser.add_parser("basic", help="Basic arithmetic operations")
+basic_parser.add_argument("operation", choices=["add", "sub", "div", "mul", "mod"])
+basic_parser.add_argument("numbers", type=float, nargs="+")
+
+arguments = parser.parse_args()
+
+if arguments.command == "basic":
+    if arguments.operation == "add":
+        res = basic_ops.add(*arguments.numbers)
+        print(f"{res:g}")
