8ce0b04adf1c2ed0535f45ed4f291d39f3de8104
Aarav Maloo <aaravmaloo06@gmail.com>
2025-10-18 19:51:21 +0530

    added operations of mul and sub

--- calculator/basic_ops.py
+++ calculator/basic_ops.py
@@ -5,4 +5,15 @@ def add(*args):
         total += i
     return total
 
+def sub(*args):
+    total = 0
+    for i in args:
+        total -= i
+    return total
+
+def mul(*args):
+    total = 1
+    for i in args:
+        total *= i
+    return total
 
