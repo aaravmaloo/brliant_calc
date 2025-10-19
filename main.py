4f12d8fef49fb857c4f9a3f6270dc03363d1551c
Aarav Maloo <aaravmaloo06@gmail.com>
2025-10-19 18:17:09 +0530

    updated main.py for adv_opers

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ basic_parser = subparser.add_parser("basic", help="Basic arithmetic operations")
 basic_parser.add_argument("operation", choices=["add", "sub", "div", "mul", "mod"])
 basic_parser.add_argument("numbers", type=float, nargs="+")
 
+
+advanced_parser = subparser.add_parser("adv", help="Advanced mathematical operations")
+advanced_parser.add_argument("operation", choices=["sin", "cos", "tan", "log", "exp", "nth", "pow"])
+advanced_parser.add_argument("numbers", type=float, nargs="+")
+
 arguments = parser.parse_args()
 
 if arguments.command == "basic":
@@ -20,5 +25,17 @@ if arguments.command == "basic":
 
     if isinstance(result, (int, float)):
         print(f"{result:g}")  
+    else:
+        print(result)
+elif arguments.command == "adv":
+    operation = arguments.operation
+    nums = arguments.numbers   
+
+    from calculator import advanced_ops
+    func = getattr(advanced_ops, operation)
+    result = func(*nums)     
+
+    if isinstance(result, (int, float)):
+        print(f"{result:g}")
     else:
         print(result)
