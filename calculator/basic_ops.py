3ed899eff1c7a0182e36d1f8393ecea55c3e3f74
Aarav Maloo <aaravmaloo06@gmail.com>
2025-10-19 17:44:42 +0530

    updated how it handles div and added mod

--- calculator/basic_ops.py
+++ calculator/basic_ops.py
@@ -1,3 +1,5 @@
+import math
+
 
 def add(*args):
     total = 0
@@ -18,9 +20,19 @@ def mul(*args):
     return total
 
 def div(*args):
-    if 0 in args:
-        print("cannot divide by zero.")
-    for i in args:
-        total = args[0]
+    if 0 in args[1:]:  
+        return "cannot divide by zero."
+    total = args[0]  
+    for i in args[1:]: 
         total /= i
     return total
+
+def mod(*args):
+    if len(args) != 2:
+        return "the mod function requires exactly two arguments (dividend, divisor)."
+    
+    dividend, divisor = args
+    if divisor == 0:
+        return "cannot divide by zero."
+    
+    return math.fmod(dividend, divisor)
